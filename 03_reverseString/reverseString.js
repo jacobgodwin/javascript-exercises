cff93179c8a80a080950b8ba5cccf891eec4ae30
Jacob Godwin <jpg174@gmail.com>
2021-12-08 15:50:48 -0500

    solve problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function(str) {
-
+const reverseString = function (str) {
+  let myArray = Array.from(str);
+  let reversed = myArray.reverse();
+  let reveresedString = reversed.join("");
+  return reveresedString;
 };
 
 // Do not edit below this line
