d5547ca19c6da1e2dc5a3cc3c576c6513f79bc48
Jacob Godwin <jpg174@gmail.com>
2021-12-09 13:16:35 -0500

    solve problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  celsius = +celsius.toFixed(1);
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let fahrenheit = temp * (9 / 5) + 32;
+  fahrenheit = +fahrenheit.toFixed(1);
+  return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
