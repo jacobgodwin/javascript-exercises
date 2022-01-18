73a5a46d41aafe2aad9735f52acfb6298aaa8513
Jacob Godwin <jpg174@gmail.com>
2022-01-18 15:22:12 -0500

    solve problem 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (item) {
+  const puncts = [".", ",", "!", "?", ";", ":"];
+  let pal = item.toLowerCase();
+  puncts.forEach(function (punct) {
+    if (pal.endsWith(punct)) {
+      let palLen = pal.length;
+      let noPunct = pal.slice(0, palLen - 1);
+      pal = noPunct;
+    }
+  });
+  if (pal.indexOf(",") > -1) {
+    let noComma = pal.split(",").join("");
+    pal = noComma;
+  }
+  if (pal.indexOf(" ") > -1) {
+    let noSpace = pal.split(" ").join("");
+    pal = noSpace;
+  }
+  let palSplit = pal.split("");
+  let revPalArray = palSplit.reverse();
+  let joinPal = revPalArray.join("");
 
+  if (pal === joinPal) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
