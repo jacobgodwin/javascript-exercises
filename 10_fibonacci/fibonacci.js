545e20e4dee2c4976eea3f363d07d3cfde194197
Jacob Godwin <jpg174@gmail.com>
2022-01-18 16:02:24 -0500

    solve problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let fib = [];
+  if (num < 0) {
+    return "OOPS";
+  }
+  for (let i = 0; i <= num; i++) {
+    if (fib.length === 0) {
+      fib.push(1, 1);
+    } else {
+      fib.push(fib[i - 1] + fib[i]);
+    }
+  }
+  return fib[num - 1];
 };
 
 // Do not edit below this line
